bc55d82a6133d5bc8effa1400261c1b52bacd767
lolstopper <pranavagrawal929@gmail.com>
2020-12-26 17:05:06 +0530

    Update agentenv.py

--- agentenv.py
+++ agentenv.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as mplPP
 #control room/ vars        
 InitialAgentNos = 100 #no of agents initially
 agents = []
+BuyingRate = 1.0
+SellingRate = 1.0
 
 year = 0 #counter of the simulation year;
 
@@ -57,6 +59,18 @@ class agent():
     def miner(self):
         self.color = 'grey'
         self.job  = 'miner'
+
+    def buyer(self):
+        global BuyingRate
+        
+        pass#looks for goods in the market
+    
+
+    def seller(self):
+        global SellingRate
+        
+        pass#looks to sell goods in the market
+    
         
 #functions
 def AgentInitialise():
@@ -117,27 +131,40 @@ def SupplyDemand():
     #at the end of every yr, when an agent attempts to sell his produce, this function is called.
     #it dictates how many of each job is present in the simulation as well
     
-    global agents, subsistence
+    global agents, subsistence, farmgoods
     subsistence = 40
+    farmgoods = 0 #farm goods in the market
     
+    for a in agents:
+
+        if a.job == 'farmer' and a.goods >= subsistence:
+            #trial implementation
+            a.goods -= subsistence
+            a.seller()
+            farmgoods += a.goods
+
+        else:
+            a.buyer()
+
+
+def Step():
+    while year <= 10: #simualation step    
+        for a in agents:
+            a.age += 1
+            if a.health <= 0 or a.age >= 100:
+                del a #delete the agent
+
+            if a.age < 18:
+                a.prod = 0        
+
 
 ModelAgent = agent()
 agents.append(ModelAgent)
 
 
-while year <= 10: #simualation step    
-    for a in agents:
-        a.age += 1
-        if a.health <= 0 or a.age >= 100:
-            del a #delete the agent
-
-        if a.age < 18:
-            a.prod = 0
-
-    if year % 10 == 0: #yields statistics for mapping
-        global farmgoods #total value of surplus farm goods in the system
-        global popn
-        popn = len(agents)
+if year % 10 == 0: #yields statistics for mapping
+    global farmgoods, popn #total value of surplus farm goods in the system
+    popn = len(agents)
         
     
     year += 1
