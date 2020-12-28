2ac49b21e6243e7e2302c2e5a3e1725787fb8ac8
lolstopper <pranavagrawal929@gmail.com>
2020-12-28 23:44:38 +0530

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,4 +1,47 @@
-def buyer(self):
+sellingprice = 10
+subsistence = 40
+farmgoods = 0
+marketrate = 0
 
-def seller(self):
+class agent():
+    def __init__(self):
+        self.credit = 100
+
+    def trade(self):
+        global subsistence
+        
+        if self.goods > subsistence:
+            self.seller()
+
+        else:
+            self.buyer()
+        
+    def buyer(self):
+        global subsistence, farmgoods, marketrate
+        farmgoods -= subsistence - self.goods
+            
+        self.buy = sellingprice
+        self.credit -= self.buy * ( subsistence - self.goods )
+        print(farmgoods)
+            
+    def seller(self):
+        global subsistence, farmgoods, marketrate
+        farmgoods += self.goods - subsistence
+
+        self.sell = sellingprice - marketrate #??
+        self.credit += self.sell * ( self.goods - subsistence )
+        print(farmgoods)
+
+        
     
+a = agent()
+a.goods = 100
+a.trade()
+
+b = agent()
+b.goods = 50
+b.trade()
+
+c = agent()
+c.goods = 10
+c.trade()
