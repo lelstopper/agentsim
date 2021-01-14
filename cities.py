3ad683c9653f5f3fd6520151ce38a1838aead45c
lolstopper <pranavagrawal929@gmail.com>
2021-01-14 23:32:00 +0530

    Update cities.py

--- cities.py
+++ cities.py
@@ -2,23 +2,38 @@ import matplotlib.pyplot as mplPP
 import random
 import numpy
 
-r = 0.1
+r = 1
 class City():
     def __init__(self):
         self.x = random.random()
         self.y = random.random()
-
-    def Neighbour():
-        global cities, knn
+        self.Neighbour()
+        self.plot()
         
-        self.neighbours = [n for n in cities if ( self.x - n.x ) ** 2 + (self.y - n.y ) ** 2 < r ** 2 and n != self ] # terrible performance
-        knn =   numpy.array([self.neighbours[        
+    def Neighbour(self):
+        global cities
+        self.neighbours = numpy.array([[n.x for n in cities if ( self.x - n.x ) ** 2 + (self.y - n.y ) ** 2 < r ** 2 and n != self  ],
+                                       [n.y for n in cities if ( self.x - n.x ) ** 2 + (self.y - n.y ) ** 2 < r ** 2 and n != self ]]) # terrible performance
+        
+    def plot(self):
+        knnx = []
+        knny = []
+        for i in (range(2 * len(self.neighbours[0]))):
+            if i % 2 == 0:
+                knnx.append(self.x)
+                knny.append(self.y)
+
+            else:
+                knnx.append(self.neighbours[0][i // 2])
+                knny.append(self.neighbours[1][i // 2])
+            mplPP.plot(knnx,knny)
+        
 
 cities = []
 cx = []
 cy = []
 
-for c in range(50):
+for c in range(100):
     c = City()
     cities.append(c)
 
@@ -28,9 +43,7 @@ def PlotSim():
         cx.append(c.x)  
         cy.append(c.y)
 
-    mplPP.scatter(cx, cy)
-    mplPP.plot()
-    
+    mplPP.scatter(cx, cy)    
     mplPP.show()
 
 PlotSim()
