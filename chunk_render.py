6492d32074941368bb77041c20d3ab5971f46fac
lolstopper <pranavagrawal929@gmail.com>
2021-01-25 12:20:25 +0530

    Create chunk_render.py

---
+++ chunk_render.py
@@ -0,0 +1,10 @@
+#chunk render implementation
+
+'''
+steps of algorithm:
+    1. store the current coords of the pointer
+    2. accept instructions to move the pointer left and right
+    3. load an area around the pointer, and unload areas left behind
+    4. run benchmarks
+
+'''
