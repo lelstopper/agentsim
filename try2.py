4a80c68ed432bef38fc06a284a116a624fa80d5e
lolstopper <pranavagrawal929@gmail.com>
2021-02-01 11:55:04 +0530

    Create try2.py

---
+++ try2.py
@@ -0,0 +1,17 @@
+'''
+simulate 'x' number of workers and try and fulfil end conditions as well as possible.
+
+current end condition:
+    * gross economic output after a set amt of time
+
+multiple AI will be used, and they will periodically interact with each other; maybe even trade
+
+each agent gets
+    * productivity influenced by taxation rate
+    * basic needs
+    * tax rate - AI definable
+    * social security - AI definable
+    *
+    
+trade rules:
+    
