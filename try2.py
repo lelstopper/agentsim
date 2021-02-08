284818fde161c8eaf79edc6dd4bd5c4bf10e7d48
lolstopper <pranavagrawal929@gmail.com>
2021-02-08 12:06:24 +0530

    Update try2.py

--- try2.py
+++ try2.py
@@ -2,33 +2,51 @@
 simulate 'x' number of workers and try and fulfil end conditions as well as possible.
 
 current end condition:
-    * gross economic output after a set amt of time
+    1. gross economic output after a set amt of time
+    1a. keep as many children alive to enable condition 1
 
-multiple AI will be used, and they will periodically interact with each other; maybe even trade
+multiple AIs will be used, and they will periodically interact with each other; maybe even trade
 
+
+
+sellers will be trying to get as many goods off their hands as possible, they will even sell at a slight loss to get rid of produce.
 each agent gets
     * productivity
         * factors ???
         
     * basic needs
     * tax rate - AI definable
+        * decline of individual income in proportion to the tax rate
+        * fixed amount to tax entire society: AI can decide who gets taxed more
+
+    * occupational role
     * social security - AI definable
-    *
-    
-trade rules:
-    *
+    * selling / buying price of goods - marketrate - AI definable
+
+children can be implemented as a challenge condition for social securities etc. as of right now they have no guardians, the state must take care of them. all new agents are children w age = 0;
+they do not produce anything until the  age of 18, they require subsistence levels of goods and their occupation is locked in from the moment theyre born. otherwise standard agents
+
 
 TODO:
     * flowchart of functions so that we can see the simulation progress
         ie. AgentInitialise -> [ MarketInitialise -> SupplyDemand ] x years
 
+    * SQL Connectivity
     * credit system
-
     * inplement a flexible market rate
+    * population changes
     * tax rate
+    * occupational changes
+        * everyone starts off as a farmer, and the AI can probably decide when to change a farmer to say, a trader.
+
+DONE:
+    * agent initilisation
 
 
- database used to store values of the occupation details to be queried
+ LIST OF POSSIBLE AI CONTROLLABLE 'NODES':
+     * tax rate ( flexible )
+     * rate of change of occupation
+     * marketrate (possible)
  '''
 
 #imports
@@ -38,16 +56,17 @@ import matplotlib.pyplot as mplPP
 import random
 
 #control room
-FirstNameMale = ['Raj', 'Mohan', 'Pranav', 'Praneel', 'Rohan', 'Anish', 'Akhil', 'Jay']
+FirstNameMale = ['Raj', 'Mohan', 'Pranav', 'Praneel', 'Rohan', 'Anish', 'Akhil', 'Jay', 'Naresh', 'Rajesh', 'Manish', 'Hitesh']
 FirstNameFemale = ['Nora', 'Shreya', 'Pramila', 'Bhavya']
-LastName = ['Agrawal', 'Rao', 'Singh', 'Yadav', 'Prasad', 'Jithendra']
+LastName = ['Agrawal', 'Rao', 'Singh', 'Yadav', 'Prasad', 'Jithendra', 'Chandra', 'Gowda', 'Rajesh']
 
 InitialAgentNos =  100 #no of agents initially
 agents = []
 
-subsistence = 50
+subsistence = 40
+MarketRate = 1
 
-r = 0.1
+r = 0.25
 
 
 #classes
@@ -119,9 +138,9 @@ class agent():
         self.neighbours = [n for n in agents if ( self.x - n.x ) ** 2 + (self.y - n.y ) ** 2 < r ** 2 and n != self and n.marker == 'buyer'] # terrible implementation
 
         NeighbourData = {
-                          'Name: ': [n.name for n in self.neighbours],
+                          'Name': [n.name for n in self.neighbours],
                           'Age': [n.age for n in self.neighbours],
-                          'Job ': [n.job for n in self.neighbours],
+                          'Job': [n.job for n in self.neighbours],
                           'Sex': [n.gender for n in self.neighbours],
                           'Goods': [n.goods for n in self.neighbours],
                           'Cash': [n.credit for n in self.neighbours],
@@ -146,7 +165,7 @@ def AgentInitialise():
         agents.append(a)
 AgentInitialise()
  
-def MarketInitialise():
+def Market1Initialise():
     
     '''
         steps to the algo
@@ -174,22 +193,76 @@ def MarketInitialise():
                 print('\n')
 
                 #check if the trade is possible:
-                if a.sell >= b.buy: #credit argument too
-                    pass
-                        #a.goods, a.buy, a.credit
-                        #b.sell, b.goods, b.credit
-                        #create a token for the trade that has taken place
+                if ( a.sell >= b.buy ) and ( b.credit > b.buy * MarketRate ): #credit argument too
+                    a.sell -= b.buy
+                    a.goods -= b.buy
+                    a.credit += ( b.buy * MarketRate )
+                    
+                    b.goods += b.buy
+                    b.credit -= ( b.buy * MarketRate )
+                    b.buy = 0
 
-MarketInitialise()
+                    a.neighbours.remove(b)
+                    pass #create a token for the trade that has taken place in the SQL Database
+                
+                elif ( a.sell < b.buy ) and (b.credit > a.sell ): #credit argument too'
+                    b.buy -= a.sell
+                    b.goods += a.sell
+                    b.credit -= ( a.sell * MarketRate )
+                    
+                    a.credit += ( a.sell * MarketRate )
+                    a.sell = 0
+                    pass #modified trade rules
+
+                else:
+                    print('The trade did not go through')
+                    pass #trade cannot go through
+
+Market1Initialise()
     
 #Data
+
+def YrUpdate():
+    '''
+    steps to the algo:
+        1. initialise the counter
+        2. start on the first year:
+            * initialise all the agents
+                * define their starting productivity values, jobs, ages, location etc.
+                
+            * define the starting subsistence rate
+            * mark each agent ( either a seller or a buyer )
+            * make a list of all the nearby buyers of each seller
+            * for each seller, attempt a trade with each nearby buyer in order of the list.
+                * buyer can not buy any more if he has run out of credits
+                * buyer can not buy any more if the seller has run out of goods 
+                * buyer will not buy any more if the subsistence level has been crossed ( this needs to be worked on )
+                * the seller can not trade any more when he runs out of goods to sell.
+                
+            * once all possible trades have been attempted, end the marketplace
+            * any agent without enough goods to reach the subsistence level will be terminated.
+                * count the agents terminated
+            * calculate the surplus
+                * tax
+
+        3. start the next year:
+            * any agent terminated is replaced, with new agent ( agent properties to be worked on next )
+            * modify all old agents
+                * change productivity values, jobs, increase age by 1, etc.
+
+            * 
+
+    '''
+    pass
+
+
 
 
 BuyerData = {'Name: ': [a.name for a in agents if a.marker == 'buyer'],
               'Age': [a.age for a in agents if a.marker == 'buyer'],
               'Job   ': [a.job for a in agents if a.marker == 'buyer'],
               'Sex': [a.gender for a in agents if a.marker == 'buyer'],
-              'Productivity': [a.prod for a in agents if a.marker == 'buyer'],
+              'Goods': [a.goods for a in agents if a.marker == 'buyer'],
               'Cash': [a.credit for a in agents if a.marker == 'buyer'],
               'Status': [a.marker for a in agents if a.marker == 'buyer'],
               
@@ -204,6 +277,7 @@ SellerData = {'Name: ': [a.name for a in agents if a.marker == 'seller'],
               'Job   ': [a.job for a in agents if a.marker == 'seller'],
               'Sex': [a.gender for a in agents if a.marker == 'seller'],
               'Productivity': [a.prod for a in agents if a.marker == 'seller'],
+              'Goods': [a.goods for a in agents if a.marker == 'seller'],
               'Cash': [a.credit for a in agents if a.marker == 'seller'],
               'Status': [a.marker for a in agents if a.marker == 'seller'],
               
@@ -220,7 +294,7 @@ AgentData = { 'Name: ': [a.name for a in agents],
               'Productivity': [a.prod for a in agents],
               'Cash': [a.credit for a in agents],
               'Status': [a.marker for a in agents],
-              
+              'Goods': [a.goods for a in agents],
               #creates a data visualisation of the simulation
             }
 
